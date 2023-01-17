3e699ab483edf9ddd405ef8d4b6ba2a533534867
Dylan Emerson <emersonds2868@gmail.com>
2023-01-17 16:37:11 -0500

    Changed Input Action Asset name, started input handling
    - Changed "DefaultPlayerMap" to "PlayerInputs" for clarity. This Input Actions Asset will be used for ALL player inputs, so there is no need to specify a default.
    - Generated a C# script for the Input Actions Asset to have access to context callbacks and such.
    - Started setting up player inputs in PlayerController.cs.
    - Attached PlayerController script to player.

--- Gloaming Shepherd/Assets/Scripts/PlayerController.cs
+++ Gloaming Shepherd/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Player Input Action Asset
+    private PlayerInputs playerInputs;
+
+    private void Awake()
+    {
+        playerInputs = new PlayerInputs();
+    }
+
+    private void OnEnable()
+    {
+        playerInputs.Default.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Default.Disable();
+    }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    /*
+    /// <summary>
+    /// This function is used to test inputs from an Input Action Asset
+    /// </summary>
+    /// <param name="ctx"></param>
+    private void InputTester(InputAction.CallbackContext ctx)
+    {
+        Debug.Log($"{ctx.action.name}");
+    }
+    */
 }
