5863d8b58063dd5349f3fe9a236caedc7d48a6a9
Dylan Emerson <emersonds2868@gmail.com>
2023-01-17 17:21:39 -0500

    Implemented player movement
    - The player can now move using the Walk input action from the Default action map in player inputs.
    - This is done by adding force to the Rigidbody component of the player that was added this commit.
    - Currently, the AddForce method is using the VelocityChange ForceMode, but this may change in the future if a better ForceMode is determined.
    - Tweaked moveSpeed and the Drag value of the rigidbody component to get smooth movement that slides to a stop when the player stops moving.

--- Gloaming Shepherd/Assets/Scripts/PlayerController.cs
+++ Gloaming Shepherd/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Player Stats
+    [SerializeField, Tooltip("How fast the player walks.")]
+    private float moveSpeed = 1f;
+
+    // Components - These are attached in the Inspector
+    private Rigidbody rb;
+
     // Player Input Action Asset
     private PlayerInputs playerInputs;
+
+    // Used to move the player from movement input action
+    private Vector3 moveVector;
 
     private void Awake()
     {
@@ -26,7 +36,10 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        
+        playerInputs.Default.Walk.performed += UpdateMoveVector;
+        playerInputs.Default.Walk.canceled += ctx => { moveVector = Vector3.zero; };
+
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -37,17 +50,33 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+        Move();
     }
 
-    /*
+    private void Move()
+    {
+        rb.AddForce(moveVector * moveSpeed, ForceMode.VelocityChange);
+    }
+
     /// <summary>
-    /// This function is used to test inputs from an Input Action Asset
+    /// Update moveVector when a new Walk vector is input by the player.
+    /// This is done in a separate method rather than FixedUpdate() so that
+    /// moveVector only creates new Vector3 when a new input is given, rather
+    /// than every FixedUpdate cycle.
+    /// </summary>
+    /// <param name="ctx">The Walk InputAction callback context</param>
+    private void UpdateMoveVector(InputAction.CallbackContext ctx)
+    {
+        moveVector = new Vector3(ctx.ReadValue<Vector2>().x, 0f, ctx.ReadValue<Vector2>().y);
+    }
+
+    /// <summary>
+    /// This function is used to test inputs from an Input Action Asset.
+    /// Gets InputAction Callback Context and displays input action name.
     /// </summary>
     /// <param name="ctx"></param>
     private void InputTester(InputAction.CallbackContext ctx)
     {
         Debug.Log($"{ctx.action.name}");
     }
-    */
 }
